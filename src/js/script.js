3bcd465dff93bf5ce374af822e854f243ff171a9
fuchiwaki_rie <0896.riri@gmail.com>
2022-06-26 16:38:54 +0900

    edit indent

--- src/js/script.js
+++ src/js/script.js
@@ -2,21 +2,24 @@
 const scroll_to_top_btn = document.querySelector('#scroll-to-top-btn');
 
 //クリックイベントを追加
-scroll_to_top_btn.addEventListener( 'click' , scroll_to_top );
+scroll_to_top_btn.addEventListener('click', scroll_to_top);
 
-function scroll_to_top(){
-	window.scroll({top: 0, behavior: 'smooth'});
+function scroll_to_top() {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth'
+  });
 };
 
 //スクロール時のイベントを追加
-window.addEventListener( 'scroll' , scroll_event );
+window.addEventListener('scroll', scroll_event);
 
-function scroll_event(){
-	
-	if(window.pageYOffset > 400){
-		scroll_to_top_btn.style.opacity = '1';
-	}else	if(window.pageYOffset < 400){
-		scroll_to_top_btn.style.opacity = '0';
-	}
-	
+function scroll_event() {
+
+  if (window.pageYOffset > 400) {
+    scroll_to_top_btn.style.opacity = '1';
+  } else if (window.pageYOffset < 400) {
+    scroll_to_top_btn.style.opacity = '0';
+  }
+
 };
