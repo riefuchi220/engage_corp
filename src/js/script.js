3b71c0f0e173214fcf4a943eae4ea4400e31144d
fuchiwaki_rie <0896.riri@gmail.com>
2022-06-26 17:12:47 +0900

    add anchor link

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,15 @@
+$(function () {
+  $('a[href^="#"]').click(function () {
+    const speed = 600;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    let position = target.offset().top;
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+});
+
+
 //ボタン
 const scroll_to_top_btn = document.querySelector('#scroll-to-top-btn');
 
